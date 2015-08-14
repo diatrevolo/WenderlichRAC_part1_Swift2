60405440904c553778ce4458768c47e0d4d1bfd9
Roberto Osorio <rjo218@gmail.com>
2015-08-14 15:19:15 -0400

    Initial Commit

---
+++ SwiftReactivePlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftReactivePlayground
+//
+//  Created by Roberto Osorio Goenaga on 8/14/15.
+//  Copyright © 2015 Roberto Osorio Goenaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
